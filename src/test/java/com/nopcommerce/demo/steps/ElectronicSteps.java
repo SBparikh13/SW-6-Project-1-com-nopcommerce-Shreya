30437baeb57fd21a60f47c1b7b0a13046e7ec97e
sbparikh13 <shrevik1309@gmail.com>
2023-05-16 23:35:07 +0100

    SW-6-Project-1-com-nopcommerce-Shreya

---
+++ src/test/java/com/nopcommerce/demo/steps/ElectronicSteps.java
@@ -0,0 +1,190 @@
+package com.nopcommerce.demo.steps;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ElectronicSteps {
+    @Given("^I am on homepage$")
+    public void iAmOnHomepage() {
+
+    }
+
+    @When("^I mouse hover on electronics$")
+    public void iMouseHoverOnElectronics() {
+    }
+
+    @And("^I select \"([^\"]*)\"$")
+    public void iSelect(String arg0) {
+    }
+
+    @Then("^User should navigated to cellphone page using \"([^\"]*)\"$")
+    public void userShouldNavigatedToCellphonePageUsing(String arg0) {
+    }
+
+    @When("^I click on List view$")
+    public void iClickOnListView() {
+    }
+
+    @And("^I Click on product name “Nokia Lumia”$")
+    public void iClickOnProductNameNokiaLumia() {
+    }
+
+    @Then("^I Verify the text \"([^\"]*)\"$")
+    public void iVerifyTheText(String arg0){
+    }
+
+    @And("^I Verify the price using \"([^\"]*)\"$")
+    public void iVerifyThePriceUsing(String arg0){
+    }
+
+    @And("^I Change quantity to \"([^\"]*)\"$")
+    public void iChangeQuantityTo(String arg0){
+    }
+
+    @And("^I Click on “ADD TO CART” tab$")
+    public void iClickOnADDTOCARTTab() {
+    }
+
+    @Then("^I Verify the success Message \"([^\"]*)\" on Top green Bar$")
+    public void iVerifyTheSuccessMessageOnTopGreenBar(String arg0) {
+    }
+
+    @And("^I close the bar clicking on the cross button\\.$")
+    public void iCloseTheBarClickingOnTheCrossButton() {
+    }
+
+    @And("^I MouseHover on Shopping cart$")
+    public void iMouseHoverOnShoppingCart() {
+    }
+
+    @And("^I Click on GO TO CART button\\.$")
+    public void iClickOnGOTOCARTButton() {
+    }
+
+    @Then("^I Verify the message \"([^\"]*)\"$")
+    public void iVerifyTheMessage(String arg0) {
+    }
+
+    @And("^I Verify the quantity is \"([^\"]*)\"$")
+    public void iVerifyTheQuantityIs(String arg0) {
+    }
+
+    @And("^I Verify the Total \"([^\"]*)\"$")
+    public void iVerifyTheTotal(String arg0) {
+    }
+
+
+    @When("^I click on checkbox of I agree with the terms of service$")
+    public void iClickOnCheckboxOfIAgreeWithTheTermsOfService() {
+    }
+
+    @And("^I Click on CHECKOUT$")
+    public void iClickOnCHECKOUT() {
+    }
+
+//    @Then("^I Verify the Text \"([^\"]*)\"$")
+//    public void iVerifyTheText(String arg0) {
+//    }
+
+    @And("^I Click on “REGISTER” tab$")
+    public void iClickOnREGISTERTab() {
+    }
+
+    @And("^I Fill the mandatory fields$")
+    public void iFillTheMandatoryFields() {
+    }
+
+    @And("^I Click on “REGISTER” Button$")
+    public void iClickOnREGISTERButton() {
+    }
+
+    @And("^I Verify the message \"([^\"]*)\" on registration page$")
+    public void iVerifyTheMessageOnRegistrationPage(String arg0) {
+    }
+
+    @And("^I Click on “CONTINUE” tab$")
+    public void iClickOnCONTINUETab() {
+    }
+
+    @And("^I Login to website$")
+    public void iLoginToWebsite() {
+    }
+
+    @And("^I Fill the rest mandatory field$")
+    public void iFillTheRestMandatoryField() {
+    }
+
+    @And("^I Click CONTINUE$")
+    public void iClickCONTINUE() {
+    }
+
+    @And("^I Click on Radio Button “Second Day Air”$")
+    public void iClickOnRadioButtonSecondDayAir() {
+    }
+
+    @And("^I Click on CONTINUE$")
+    public void iClickOnCONTINUE() {
+    }
+
+    @And("^I Select Radio Button “Credit Card”$")
+    public void iSelectRadioButtonCreditCard() {
+    }
+
+    @And("^I click on Payment Continue$")
+    public void iClickOnPaymentContinue() {
+    }
+
+    @And("^I Select “Visa” From Select credit card dropdown$")
+    public void iSelectVisaFromSelectCreditCardDropdown() {
+    }
+
+    @And("^I Fill all the details$")
+    public void iFillAllTheDetails() {
+    }
+
+    @And("^I click on next Payment Continue$")
+    public void iClickOnNextPaymentContinue() {
+    }
+
+    @Then("^I Verify Payment Method is \"([^\"]*)\"$")
+    public void iVerifyPaymentMethodIs(String arg0){
+    }
+
+    @And("^I Verify Shipping Method is \"([^\"]*)\"$")
+    public void iVerifyShippingMethodIs(String arg0){
+    }
+
+    @And("^I Verify Total is \"([^\"]*)\"$")
+    public void iVerifyTotalIs(String arg0) {
+    }
+
+    @And("^I Click on CONFIRM$")
+    public void iClickOnCONFIRM() {
+    }
+
+    @Then("^I Verify the Text head \"([^\"]*)\"$")
+    public void iVerifyTheTextHead(String arg0){
+    }
+
+    @Then("^I Verify the Success message \"([^\"]*)\"$")
+    public void iVerifyTheSuccessMessage(String arg0) {
+    }
+
+    @And("^I Click on CONTINUE Button$")
+    public void iClickOnCONTINUEButton() {
+    }
+
+    @Then("^I Verify the text using \"([^\"]*)\"$")
+    public void iVerifyTheTextUsing(String arg0){
+    }
+
+    @When("^I Click on “Logout” link$")
+    public void iClickOnLogoutLink() {
+    }
+
+    @Then("^I Verify the URL is \"([^\"]*)\"$")
+    public void iVerifyTheURLIs(String arg0) {
+    }
+}
